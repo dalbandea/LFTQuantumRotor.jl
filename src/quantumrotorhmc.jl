4e24f5991da8e3cd50c06f91659b32022df8fefa
dalbandea <albandeadavid@gmail.com>
2023-10-17 10:52:45 -0500

    reorganize file

--- src/quantumrotorhmc.jl
+++ src/quantumrotorhmc.jl
@@ -1,5 +1,9 @@
 import LFTSampling: sample!, generate_momenta!, Hamiltonian, update_momenta!, update_fields!
 
+###########################
+# Abstract Discretization #
+###########################
+
 function sample!(qrws::QuantumRotor, samplerws::AbstractHMC; do_winding = false)
     hmc!(qrws, samplerws)
     do_winding && winding_step!(qrws)
@@ -11,22 +15,6 @@ function generate_momenta!(qrws::QuantumRotor, hmcws::QuantumRotorHMC, disc::Typ
     for i in 1:length(hmcws.mom)
         hmcws.mom[i] = randn(qrws.PRC) * hmcws.params.width
     end
-    return nothing
-end
-
-function generate_momenta!(qrws::QuantumRotor, hmcws::QuantumRotorHMC, disc::Type{StAngleDifferenceDiscretization})
-    for i in 1:length(hmcws.mom)-1
-        hmcws.mom[i] = randn(qrws.PRC) * hmcws.params.width
-    end
-    hmcws.mom[qrws.params.iT] = zero(qrws.PRC)
-    return nothing
-end
-
-function generate_momenta!(qrws::QuantumRotor, hmcws::QuantumRotorHMC, disc::Type{StAngleDifferenceTrivMapDiscretization})
-    for i in 1:length(hmcws.mom)-1
-        hmcws.mom[i] = randn(qrws.PRC) * hmcws.params.width
-    end
-    hmcws.mom[qrws.params.iT] = zero(qrws.PRC)
     return nothing
 end
 
@@ -46,6 +34,52 @@ function theta_force(qrws::QuantumRotor, BC::Type{OpenBC})
         return one(qrws.PRC)*qrws.params.theta
     end
 end
+
+function update_momenta!(qrws::QuantumRotor, epsilon, hmcws::QuantumRotorHMC)
+
+    # Load phi force
+    force!(qrws, hmcws) 
+
+    # Update phi momenta
+    hmcws.mom .= hmcws.mom .+ epsilon .* hmcws.frc
+
+    return nothing
+end
+
+function update_fields!(qrws::QuantumRotor, epsilon, hmcws::QuantumRotorHMC)
+    # Update phi field
+    qrws.phi .= qrws.phi .+ epsilon .* hmcws.mom ./ hmcws.params.width^2
+    return nothing
+end
+
+function flip_momenta_sign!(hmcws::QuantumRotorHMC)
+    hmcws.mom .= .- hmcws.mom
+    return nothing
+end
+
+
+
+
+###################################
+# Angle Difference Discretization #
+###################################
+
+function generate_momenta!(qrws::QuantumRotor, hmcws::QuantumRotorHMC, disc::Type{StAngleDifferenceDiscretization})
+    for i in 1:length(hmcws.mom)-1
+        hmcws.mom[i] = randn(qrws.PRC) * hmcws.params.width
+    end
+    hmcws.mom[qrws.params.iT] = zero(qrws.PRC)
+    return nothing
+end
+
+function generate_momenta!(qrws::QuantumRotor, hmcws::QuantumRotorHMC, disc::Type{StAngleDifferenceTrivMapDiscretization})
+    for i in 1:length(hmcws.mom)-1
+        hmcws.mom[i] = randn(qrws.PRC) * hmcws.params.width
+    end
+    hmcws.mom[qrws.params.iT] = zero(qrws.PRC)
+    return nothing
+end
+
 
 function force!(qrws::QuantumRotor, hmcws::QuantumRotorHMC, disc::Type{D}, BC::Type{OpenBC}) where D <: AbstractAngleDifferenceDiscretization
 
@@ -106,25 +140,3 @@ function boundary_force!(qrws::QuantumRotor, hmcws::QuantumRotorHMC, disc::Type{
 end
 
 
-function update_momenta!(qrws::QuantumRotor, epsilon, hmcws::QuantumRotorHMC)
-
-    # Load phi force
-    force!(qrws, hmcws) 
-
-    # Update phi momenta
-    hmcws.mom .= hmcws.mom .+ epsilon .* hmcws.frc
-
-    return nothing
-end
-
-
-function update_fields!(qrws::QuantumRotor, epsilon, hmcws::QuantumRotorHMC)
-    # Update phi field
-    qrws.phi .= qrws.phi .+ epsilon .* hmcws.mom ./ hmcws.params.width^2
-    return nothing
-end
-
-function flip_momenta_sign!(hmcws::QuantumRotorHMC)
-    hmcws.mom .= .- hmcws.mom
-    return nothing
-end
